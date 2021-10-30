704331a71d2398a4b046264cb4728e973d926770
KingBlackCow <sgs1159@naver.com>
2021-10-30 21:21:03 +0900

    필드 동기화 적용

--- src/test/java/hello/advanced/trace/logtrace/FieldHelloTraceV2Test.java
+++ src/test/java/hello/advanced/trace/logtrace/FieldHelloTraceV2Test.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FieldLogTraceTest {
+class FieldHelloTraceV2Test {
     FieldLogTrace trace = new FieldLogTrace();
 
     @Test
